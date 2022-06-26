7f43ed52793ade4b9ef67de1146553824d74f854
shubham kamble <196shubham@gmail.com>
2022-06-26 16:31:21 +0530

    updated the file

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting machine learning demo project"
+    return "CI-CD Pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
