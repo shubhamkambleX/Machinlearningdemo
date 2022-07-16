5b648127245f2ef27a9723d09bccd1eae3e25c2f
shubham kamble <196shubham@gmail.com>
2022-07-16 21:40:56 +0530

    updated the files

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -22,9 +22,10 @@ class HousingException(Exception):
         
         """
         _,_,exec_tb = error_detail.exc_info()
-        line_number = exec_tb.tb_frame.f_lineno
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scripts: [{file_name} at line number: [{line_number}] error message: [{error_message}]]"
+        error_message = f"Error occured in scripts: [{file_name} at exception block line number: [{exception_block_line_number}] and at main line number: [{try_block_line_number}] error message: [{error_message}]]"
         return error_message
     
     
